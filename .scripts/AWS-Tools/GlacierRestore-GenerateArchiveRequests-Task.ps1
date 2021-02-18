db3f3a0eea3c024089bdc27c9499c3b351b36301
Christof Senn <christof.senn@gmail.com>
2021-02-18 17:00:46 +0100

    dynamically parse ArchiveDescription as json or xml

--- .scripts/AWS-Tools/GlacierRestore-GenerateArchiveRequests-Task.ps1
+++ .scripts/AWS-Tools/GlacierRestore-GenerateArchiveRequests-Task.ps1
@@ -28,28 +28,65 @@ While ($true) {
     $files = Get-ChildItem $PendingDirectory -Filter $pattern
     If ($files.Count -gt 0) {
       $file = $(Move-ItemToDirectory -LiteralPath $files[0].FullName -Destination $ProcessingDirectory -Force -PassThru -Verbose:$Verbose).FullName
+      Out-Log "Received $file" -Level Verbose
       Try {
         $config = Read-JsonFile -Path $file -Verbose:$Verbose
-        
+        Out-Log "Parsed $file" -Level Verbose
+        Out-Log "Start with inventory $($config.InventoryFile)" -Level Verbose
+
         Read-JsonFile $config.InventoryFile `
           | Select-Object -ExpandProperty ArchiveList `
-          | ForEach-Object { @{Archive=$_; Description=$(ConvertFrom-Json -InputObject $_.ArchiveDescription)} } `
-          | Where-Object { $_.Description.type -eq 'file' } `
-          | Invoke-Script { 
-            "Retieved inventory of vault '$($config.VaultName)' containing $($_.Count) archives" | Out-Log -Level Information | Write-Host
-            } `
           | ForEach-Object {
-            $nextTaskFile = Join-Path $NextTaskDirectory "request-archive-[obj#$(Get-StringStart -InputString $_.Archive.ArchiveId -Length $env:MaxIdSize)].json"
+            $archiveId=$($_.ArchiveId);
+            $size=$_.Size;
+            $sha256TreeHash=$_.SHA256TreeHash;
+            $desc=($_.ArchiveDescription -join "").Trim()
+
+            function parse_json() {
+              Out-Log "Parsing ArchiveDescription as json: '$desc'" -Level Verbose
+              $d=$(ConvertFrom-Json -InputObject $desc)
+              @{Type=$d.type; Path=$d.path}
+            }
+
+            function parse_xml() {
+              Out-Log "Parsing ArchiveDescription as xml: '$desc'" -Level Verbose
+              $base64=$(Select-Xml -Content $desc -XPath "//m/p").ToString()
+              $path=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64))
+              Out-Log "retrieved path from xml: '$path'" -Level Verbose
+              @{Type="file"; Path=$path}
+            }
+
+            function handle_default() {
+              Out-Log "Unknown format of ArchiveDescription: '$desc'" -Level Warning
+              @{Type="unknown"; Path="undefined"}
+            }
+
+            switch ($desc[0]) {
+              '{' { $f=parse_json }
+              '<' { $f=parse_xml }
+              default { $f=handle_default }
+            }
+
+            @{
+              ArchiveId=$archiveId;
+              Size=$size;
+              SHA256TreeHash=$sha256TreeHash;
+              Path=$f.Path;
+              Type=$f.Type;
+            } } `
+          | Where-Object { $_.Type -eq 'file' } `
+          | ForEach-Object {
+            $nextTaskFile = Join-Path $NextTaskDirectory "request-archive-[obj#$(Get-StringStart -InputString $_.ArchiveId -Length $env:MaxIdSize)].json"
             "Creating Task File: $nextTaskFile" | Out-Log -Level Information | Write-Host
             $config `
               | Get-ShallowCopy -ExcludeProperty JobId, InventoryFile `
-              | Add-Member ArchivePath $_.Description.path -PassThru -Verbose:$Verbose `
-              | Add-Member ArchiveId $_.Archive.ArchiveId -PassThru -Verbose:$Verbose `
-              | Add-Member Size $_.Archive.Size -PassThru -Verbose:$Verbose `
-              | Add-Member SHA256Hash $_.Archive.SHA256TreeHash -PassThru -Verbose:$Verbose `
+              | Add-Member ArchivePath $_.Path -PassThru -Verbose:$Verbose `
+              | Add-Member ArchiveId $_.ArchiveId -PassThru -Verbose:$Verbose `
+              | Add-Member Size $_.Size -PassThru -Verbose:$Verbose `
+              | Add-Member SHA256Hash $_.SHA256TreeHash -PassThru -Verbose:$Verbose `
               | Write-JsonFile -Path $nextTaskFile -Verbose:$Verbose
             }
-        
+
         Move-ItemToDirectory -LiteralPath $file -Destination $SucceessDirectory -Force -Verbose:$Verbose
       }
       Catch {
